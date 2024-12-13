2254ba1cd83a546947d98b3aa16ff144cb609708
cclvi256 <i@cclvi.cc>
2024-12-13 08:45:24 +0800

    Add LeetCode problem 205
    This is solved in Go, by maintaining a map. If there exists some
    elements can map to 2 values, this is not isomorphic at this direction.
    
    A pair is isomorphic iff this relationship is bi-directional.

---
+++ go/lc205.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func isIsomorphic(s string, t string) bool {
+	return unidirectional_isomprphic(s, t) && unidirectional_isomprphic(t, s)
+}
+
+func unidirectional_isomprphic(s string, t string) bool {
+	letter_map := make(map[byte]byte)
+
+	for index, letter := range s {
+		if mapped_letter, found := letter_map[byte(letter)]; found {
+			if mapped_letter != t[index] {
+				return false
+			}
+		} else {
+			letter_map[byte(letter)] = t[index]
+		}
+	}
+
+	return true
+}
+
+func lc205_run() {
+	
+}
