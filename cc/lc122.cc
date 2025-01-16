52c1b7156fe3ada1d2cc3d8e29afcb05650acfc7
cc <i@cclvi.cc>
2025-01-16 23:12:45 +0800

    122

---
+++ cc/lc122.cc
@@ -0,0 +1,13 @@
+#include <cstddef>
+#include <vector>
+
+int maxProfit(std::vector<int> &prices) {
+  int profit = 0;
+  for (size_t i = 1; i < prices.size(); i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
+    }
+  }
+
+  return profit;
+}
