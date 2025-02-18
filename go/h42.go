97a7fba0ac4940872bc7714ef40d03c7fc7e780d
cc <i@cclvi.cc>
2025-02-18 20:00:02 +0800

    42

---
+++ go/h42.go
@@ -0,0 +1,28 @@
+package main
+
+func trap(height []int) int {
+	left, right := 0, len(height)-1
+
+	max_left, max_right := height[left], height[right]
+	result := 0
+
+	for left < right {
+		if max_left < max_right {
+			left++
+			max_left = max(max_left, height[left])
+			possible_water_level := min(max_left, max_right)
+			if height[left] < possible_water_level {
+				result += possible_water_level - height[left]
+			}
+		} else {
+			right--
+			max_right = max(max_right, height[right])
+			possible_water_level := min(max_left, max_right)
+			if height[right] < possible_water_level {
+				result += possible_water_level - height[right]
+			}
+		}
+	}
+
+	return result
+}
